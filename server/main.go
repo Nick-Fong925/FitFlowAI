f5062b9d02769f58523f876a1eccf89988693b9b
Nick Fong <nicholasfong1120@gmail.com>
2023-11-15 22:36:10 -0800

    added the register functionality

--- server/main.go
+++ server/main.go
@@ -2,13 +2,18 @@
 // run command: go run main.go
 // sql data base name: Nick
 // pw: 19701120Nfong!
+
 package main
+
 import (
-	_ "github.com/microsoft/go-mssqldb"
 	"database/sql"
-	"context"
-	"log"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	_ "github.com/microsoft/go-mssqldb"
+	"github.com/rs/cors"
+	"context"
 )
 
 var db *sql.DB
@@ -35,12 +40,27 @@ func main() {
 	}
 	fmt.Printf("Connected!")
 
+	/*
 	err = createUserTable()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	*/
+
+	// Use the cors middleware to enable CORS
+	c := cors.Default()
+
+	// Create a new HTTP handler with the cors middleware
+	handler := c.Handler(http.DefaultServeMux)
+
+	// Register the "/register" route
+	http.HandleFunc("/register", registerHandler)
+
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
 
+/*
 func createUserTable() error {
 	// SQL statement to create User table
 	query := `
@@ -60,3 +80,40 @@ func createUserTable() error {
 	fmt.Println("User table created successfully!")
 	return nil
 }
+*/
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse the request body
+	var registrationData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&registrationData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Insert the user data into the database
+	err := insertUser(registrationData.Email, registrationData.Password)
+	if err != nil {
+		http.Error(w, "Failed to register user", http.StatusInternalServerError)
+		return
+	}
+
+	// Send a success response
+	w.WriteHeader(http.StatusOK)
+}
+
+func insertUser(email, password string) error {
+	// SQL statement to insert user into the User table
+	query := `
+		INSERT INTO [User] (Name, Password)
+		VALUES ($1, $2);
+	`
+
+	// Execute the query
+	_, err := db.ExecContext(context.Background(), query, email, password)
+	return err
+}
+
