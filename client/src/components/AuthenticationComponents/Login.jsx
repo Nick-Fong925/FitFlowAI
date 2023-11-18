6ab35a28049b8df39aa86a2cda8c51dbfd2da37c
Nick Fong <nicholasfong1120@gmail.com>
2023-11-17 18:50:55 -0800

    added login functionality with detection of correct password and whether username exists

--- client/src/components/AuthenticationComponents/Login.jsx
+++ client/src/components/AuthenticationComponents/Login.jsx
@@ -1,11 +1,43 @@
 import { Link } from "react-router-dom";
 
 function Login() {
+
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+  
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      if (response.ok) {
+        alert("Login successful!");
+        // Redirect or perform other actions for successful login
+      } else {
+        const errorText = await response.text();
+        alert(`Login failed: ${errorText}`);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      alert("Login failed");
+    }
+  }
+  
+
   return (
     <div className="flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-600">
               Email
